d51578c6ca3bb99e5aa4b9d0ff29d61d2b4bb43e
Маклачков Александр <aleksandr.maklachkov@gazprombank.ru>
2024-01-15 18:34:06 +0300

    Css modules. Изоляция стилей

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Webpack-Microfrontends-Monorepo-SP
+
+https://www.youtube.com/watch?v=acAH2_YT6bs
+
+https://github.com/utimur/webpack-course
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,12 +5,24 @@ import { BuildOptions } from './types/types';
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
 
+  const cssLoaderWithModules = {
+    loader: 'css-loader',
+    options: {
+      // modules: true или объект с настройками - позволяет организовать CSS modules изоляцию стилей
+      modules: {
+        // В dev-режиме классы будут состоять из пути, имени файла и названия класса
+        // В prod-режиме - хэш на основе контента из 8 символов
+        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+      },
+    },
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     // Именно тут порядок важен. Лоадеры будут вызываться с конца: вначале отработает sass-loader, он передаст код css-loader-у, потом код будет передан style-loader
     // Без использования mini-css-extract-plugin стили будут помещены в js-код в сборке (это сделает style-loader). mini-css-extract-plugin позволяет собирать стили в отдельные css-файлы
     // use: ['style-loader', 'css-loader', 'sass-loader'],
-    use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+    use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, cssLoaderWithModules, 'sass-loader'],
   };
 
   const tsLoader = {
